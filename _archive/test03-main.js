77c052725f73cab2a49c6db6fea4d29a00d3a9b1
Eric Posas <ericposas06@gmail.com>
2017-04-07 14:52:06 -0500

    added new elements for testing

--- _archive/test03-main.js
+++ _archive/test03-main.js
@@ -3,20 +3,12 @@
 
 function init(){
   //new main.js file edited -- 04.07.2017
-  var stage = new Stage(300,600,'Test Ad'); // our 'stage' instance
+  var stage = new Stage(300,250,'Test Ad'); // our 'stage' instance
   var bg = new BG(); //bg class instance
-  //var t1 = new Square({id:'t1',image:'1.png',x:1,y:1}); 
-  var f1 = {
-    t1: new Square({id:'t1',image:'1.png',x:1,y:1}),
-    t2: new Square({id:'t2',image:'2.png',x:1,y:1}),
-    t3: new Square({id:'t3',image:'3.png',x:1,y:1})
-  };
-  var f2 = {
-    t4: new Square({id:'t4',image:'4.png',x:1,y:1})
-  };
+  var t1 = new Square({id:'t1',image:'1.png',x:1,y:1}); // text 01
   var logo = new Square({id:'logo',image:'logo.png'}); // logo graphic
   var outline = new Outline(); // ad 'outline' overlay -- click area
-  stage.add(bg).add(f1.t1).add(f1.t2).add(f1.t3).add(logo).add(outline); // chaining! 
+  stage.add(bg).add(t1).add(logo).add(outline); // chaining! 
   
   /* ANIMATION */
   // dynamically set animation function to be called from adstage once all images are loaded 
